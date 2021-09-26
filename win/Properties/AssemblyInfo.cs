a1a013db44f3967456f83738d4fc03f150bcf3ed
icrescenti <icrescenti@terkstudios.com>
2021-09-26 22:15:36 +0200

    properties

--- win/Properties/AssemblyInfo.cs
+++ win/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataShardPort")]
+[assembly: AssemblyTitle("DataPort")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataShardPort")]
+[assembly: AssemblyCompany("TerkStudios")]
+[assembly: AssemblyProduct("DataPort")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
