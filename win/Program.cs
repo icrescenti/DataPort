dc127e6902e5c5b9627ccb9d67ab1f4708d9e3fb
icrescenti <icrescenti@terkstudios.com>
2021-09-26 21:53:04 +0200

    Initial commit

---
+++ win/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace DataShardPort
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            while (true)
+            {
+                var shards = DriveInfo.GetDrives().Where(drive => drive.IsReady && drive.DriveType == DriveType.Removable);
+
+                foreach (DriveInfo shard in shards)
+                {
+                    if (shard.DriveType.ToString() == "Removable")
+                    {
+                        string[] lines = File.ReadAllText(shard.RootDirectory + "_").Split('\n');
+                        foreach (string line in lines)
+                        {
+                            Process p = new Process();
+                            p.StartInfo = new ProcessStartInfo(shard.RootDirectory + line.Replace("\r", String.Empty));
+                            p.StartInfo.WorkingDirectory = shard.RootDirectory.ToString();
+                            p.StartInfo.CreateNoWindow = true; 
+                            p.StartInfo.UseShellExecute = true;
+                            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                            p.Start();
+                        }
+
+                        showContents nw = new showContents(shard);
+                        nw.ShowDialog();
+                    }
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ win/showContents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataShardPort
+{
+    public partial class showContents : Form
+    {
+        private DriveInfo shard;
+        public showContents(DriveInfo shard)
+        {
+            this.shard = shard;
+            InitializeComponent();
+        }
+
+        private void showContents_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                webBrowser1.Url = new Uri("file:///" + shard.RootDirectory + "index.html");
+            }
+            catch { }
+        }
+
+        private void iloop_Tick(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(shard.RootDirectory.ToString()))
+                this.Close();
+        }
+    }
+}
